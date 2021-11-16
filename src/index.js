5041b3fec0815a3332f172debd41d75a0d36544d
eric tush <erotush77@gmail.com>
2021-11-16 11:06:32 +0300

    Changed Browser Router to Hash Router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-   <BrowserRouter>
+  <React.StrictMode>
+   <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
